f7c3d359d18ca9a274cfdf0d6ff605de92f7f5c3
lamnguyen3777 <73375708+lamnguyen3777@users.noreply.github.com>
2022-05-29 12:35:52 +0700

    Update index.tsx

--- src/components/UserDetail/index.tsx
+++ src/components/UserDetail/index.tsx
@@ -22,28 +22,28 @@ export function UserDetail (props: IUserDetailProps) {
                             alt="Noti"/>
                     </button>
                     <div className="userdetail__panel--left-name" >
-                        <span className="userdetail__panel--left-name-user">Nguyễn Hoàng Phúc</span>
+                        <span className="userdetail__panel--left-name-user">Nguyễn Nhật Lâm</span>
                     </div>
                 </div>
                 <Form className="userdetail__panel--form">
                   <section  className="userdetail__panel--form-left">
                     <Form.Item>
                         <p className="userdetail__panel--form-title">Tên người dùng</p>
-                        <Input  placeholder={"Nguyễn Hoàng Phúc"} className="userdetail__panel--form-input"/>
+                        <Input  placeholder={"Nguyễn Nhật Lâm"} className="userdetail__panel--form-input"/>
                     </Form.Item>
                     <Form.Item>
                         <p className="userdetail__panel--form-title">Số điện thoại</p>
-                        <Input  placeholder={"0855055435"} className="userdetail__panel--form-input"/>
+                        <Input  placeholder={"0339936377"} className="userdetail__panel--form-input"/>
                     </Form.Item>
                     <Form.Item>
                         <p className="userdetail__panel--form-title">Email</p>
-                        <Input  placeholder={"hoangphucdeveloper@gmail.com"} className="userdetail__panel--form-input"/>
+                        <Input  placeholder={"lamnguyen3777@gmail.com"} className="userdetail__panel--form-input"/>
                     </Form.Item>
                   </section>
                   <section  className="userdetail__panel--form-right" >
                     <Form.Item>
                         <p className="userdetail__panel--form-title">Tên đăng nhập</p>
-                        <Input  placeholder={"hoangphucdev"} className="userdetail__panel--form-input"/>
+                        <Input  placeholder={"lamnguyen3777"} className="userdetail__panel--form-input"/>
                     </Form.Item>
                     <Form.Item>
                         <p className="userdetail__panel--form-title">Mật khẩu</p>
